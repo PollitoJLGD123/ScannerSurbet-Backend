e8cf6f8cb1bbd242dbf28bc63a2d8317ed6a508b
Jose Luis <joseluisjlgd123@gmail.com>
2025-05-18 20:34:01 -0500

    Cambiar config para leer .env

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,14 +2,14 @@ import { Secret } from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.PORT || '4002';
-export const JWT_SECRET: Secret = process.env.JWT_SECRET || 'superSecretKey123';
-export const JWT_EXPIRATION= process.env.JWT_EXPIRATION || '24h';
+export const PORT = process.env.PORT || '';
+export const JWT_SECRET: Secret = process.env.JWT_SECRET || '';
+export const JWT_EXPIRATION= process.env.JWT_EXPIRATION || '';
 
-export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_PORT = process.env.DB_PORT || '3306';
-export const DB_NAME = process.env.DB_NAME || 'db_scannersurbet';
-export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASS = process.env.DB_PASS || 'root';
+export const DB_HOST = process.env.DB_HOST || '';
+export const DB_PORT = process.env.DB_PORT || '';
+export const DB_NAME = process.env.DB_NAME || '';
+export const DB_USER = process.env.DB_USER || '';
+export const DB_PASS = process.env.DB_PASS || '';
 export const SCRAPING_USER_NAME = process.env.SCRAPING_USER_NAME || '';
 export const SCRAPING_USER_PASS = process.env.SCRAPING_USER_PASS || '';
